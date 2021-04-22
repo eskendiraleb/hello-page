3941e3cc701d65c3f662f3b34fc2d5297eb82fa6
Eskindir <eskendiraleb@gmail.com>
2021-04-22 00:07:52 -0500

    Initial commit

---
+++ src/main/java/event/launchcode/hellopage/HelloPageApplication.java
@@ -0,0 +1,13 @@
+package event.launchcode.hellopage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/event/launchcode/hellopage/HelloPageApplicationTests.java
@@ -0,0 +1,13 @@
+package event.launchcode.hellopage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
