274bea4c9aa140ebf4e773bed3f899ad1c638c1d
Eskindir <eskindiraleb@yahoo.com>
2021-04-22 00:26:20 -0500

    add hello page

--- src/main/java/event/launchcode/hellopage/HelloPageApplication.java
+++ src/main/java/event/launchcode/hellopage/HelloPageApplication.java
@@ -1,5 +1,6 @@
 package event.launchcode.hellopage;
 
+import event.launchcode.hellopage.controller.HomeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class HelloPageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloPageApplication.class, args);
+//		System.out.println(HomeController.hello());
 	}
 
 }
